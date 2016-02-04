310fd768e4a705c57e3d77bb8ebf5f610e3b1632
david kaye <dfkaye@yahoo.com>
2016-02-04 11:07:30 -0800

    SISRP-13315:placeholder:redirectToToolbox()

--- src/assets/javascripts/angular/controllers/widgets/delegateLinkingController.js
+++ src/assets/javascripts/angular/controllers/widgets/delegateLinkingController.js
@@ -34,6 +34,7 @@ angular.module('calcentral.controllers').controller('DelegateLinkingController',
 
   var showLinkedStudents = function() {
     // TODO: We're waiting on design and text, SISRP-11142
+    // apiService.util.redirectToToolbox();
   };
 
   $scope.getTermsAndConditions = function() {
