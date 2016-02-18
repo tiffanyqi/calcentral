3f4946742b1d605e435295b0e40934a93fc8dc22
david kaye <dfkaye@yahoo.com>
2016-02-18 11:09:20 -0800

    SISRP-15107:show fallback for profile img when student viewed by delegate + new css and template refactoring for delegate student img + use data-ng-attr-alt

--- src/assets/javascripts/angular/controllers/widgets/delegateStudentsController.js
+++ src/assets/javascripts/angular/controllers/widgets/delegateStudentsController.js
@@ -4,7 +4,7 @@ var _ = require('lodash');
 var angular = require('angular');
 
 /**
- * Controller gets student linked to delegate user account
+ * Controller gets students linked to a delegate user account
  */
 angular.module('calcentral.controllers').controller('DelegateStudentsController', function(apiService, adminFactory, delegateFactory, $scope) {
   $scope.delegateStudents = {
@@ -50,32 +50,11 @@ angular.module('calcentral.controllers').controller('DelegateStudentsController'
     $scope.showPhoneInPersonPrivilegesMessage = $scope.showPhoneInPersonPrivilegesMessage || (!viewable && student.privileges[phone]);
   };
 
-  /**
-   * TODO:
-   * REPLACE THIS PROPERTY-SETTING METHOD WITH BACK-END PROPERTY SETTING ON THE
-   * STUDENT OBJECT.
-   *
-   * setStudentPhotoApi() adds the `profilePhotoUrl` property for a fictitious
-   * API to retrieve a delegate student's profile photo.
-   *
-   * For testing purposes, you can commment out the single assignment to display
-   * the img-not-available case in the template.
-   */
-  var setStudentPhotoApi = function(student) {
-    student.profilePhotoApi = '/api/student/photo/' + student.uid;
-  };
-
   var getStudents = function() {
     return delegateFactory.getStudents().then(function(data) {
       angular.extend($scope, _.get(data, 'data.feed'));
 
       var students = _.get(data, 'data.feed.students');
-      /**
-       * TODO: replace setStudentPhotoApi() function with back-end photo URL
-       * property (see above).
-       */
-      _.each(students, setStudentPhotoApi);
-
       _.each(students, setDelegateAccess);
       $scope.students = students;
       $scope.delegateStudents.isLoading = false;
