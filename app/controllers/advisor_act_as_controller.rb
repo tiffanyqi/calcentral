3f793c9d0bda3662f2f2a66eadacaf584ba4b1ef
Jason Miller <jason@redconfetti.com>
2016-03-01 16:48:56 -0800

    SISRP-7689 - Fixes references to Pundit::NotAuthorizedError exception class

--- app/controllers/advisor_act_as_controller.rb
+++ app/controllers/advisor_act_as_controller.rb
@@ -12,9 +12,9 @@ class AdvisorActAsController < ActAsController
       user_id = current_user.real_user_id
       user_attributes = HubEdos::UserAttributes.new(user_id: user_id).get
       authorized = user_attributes && user_attributes[:roles] && user_attributes[:roles][:advisor]
-      raise NotAuthorizedError.new("User #{user_id} is not an Advisor and thus cannot view-as #{uid_param}") unless authorized
+      raise Pundit::NotAuthorizedError.new("User #{user_id} is not an Advisor and thus cannot view-as #{uid_param}") unless authorized
     else
-      raise NotAuthorizedError.new 'We cannot confirm your role as an Advisor because Campus Solutions is unavailable. Please contact us if the problem persists.'
+      raise Pundit::NotAuthorizedError.new 'We cannot confirm your role as an Advisor because Campus Solutions is unavailable. Please contact us if the problem persists.'
     end
   end
 end
