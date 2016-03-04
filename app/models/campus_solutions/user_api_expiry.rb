9a73acfd526a21999e7516ef2014aff67244992c
Paul Kerschen <kerschen@berkeley.edu>
2016-03-04 10:46:29 -0800

    SISRP-13248 Wire up additional expiry behavior to JMS listener

--- app/models/campus_solutions/user_api_expiry.rb
+++ app/models/campus_solutions/user_api_expiry.rb
@@ -1,6 +1,7 @@
 module CampusSolutions
   module UserApiExpiry
     def self.expire(uid=nil)
+      HubEdos::MyStudent.expire uid
       User::Api.expire uid
     end
   end
--- app/models/campus_solutions/user_api_updating_model.rb
+++ app/models/campus_solutions/user_api_updating_model.rb
@@ -3,7 +3,6 @@ module CampusSolutions
     def passthrough(model_name, params)
       proxy = model_name.new({user_id: @uid, params: params})
       result = proxy.get
-      PersonDataExpiry.expire @uid
       UserApiExpiry.expire @uid
       result
     end
