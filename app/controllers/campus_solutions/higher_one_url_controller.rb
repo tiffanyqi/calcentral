20031939099a1151343a180c9aead898124fb202
John Crossman <johncrossman@berkeley.edu>
2016-03-04 20:49:32 -0800

    SISRP-15658, HigherOne GET URL API to optional delegate_uid

--- app/controllers/campus_solutions/higher_one_url_controller.rb
+++ app/controllers/campus_solutions/higher_one_url_controller.rb
@@ -2,8 +2,15 @@ module CampusSolutions
   class HigherOneUrlController < CampusSolutionsController
 
     def get
-      model = CampusSolutions::MyHigherOneUrl.from_session(session)
+      model = CampusSolutions::MyHigherOneUrl.from_session proxy_args
       render json: model.get_feed_as_json
+    end
+
+    private
+
+    def proxy_args
+      delegate_uid = session[SessionKey.original_delegate_user_id]
+      delegate_uid ? session.merge(delegate_uid: delegate_uid) : session
     end
 
   end
--- spec/models/campus_solutions/higher_one_url_spec.rb
+++ spec/models/campus_solutions/higher_one_url_spec.rb
@@ -4,25 +4,46 @@ describe CampusSolutions::HigherOneUrl do
 
   shared_examples 'a proxy that gets data' do
     subject { proxy.get }
+
     it_should_behave_like 'a simple proxy that returns errors'
     it_behaves_like 'a proxy that properly observes the SIR feature flag'
     it_behaves_like 'a proxy that got data successfully'
+
     it 'returns data with the expected structure' do
       expect(subject[:feed][:root][:higherOneUrl][:url]).to be
     end
   end
 
   context 'mock proxy' do
-    let(:proxy) { CampusSolutions::HigherOneUrl.new(fake: true, user_id: user_id) }
+    let(:proxy) { CampusSolutions::HigherOneUrl.new options }
+
     subject { proxy.get }
-    it_should_behave_like 'a proxy that gets data'
-    it 'should get specific mock data' do
-      expect(subject[:feed][:root][:higherOneUrl][:url].strip).to eq 'https://commerce.cashnet.com/UCBpaytest?eusername=8062064084e9a8dff7a181266a3ed11e28b80eb30ab4fd84b9bc4de92394d884'
+
+    context 'pass EMPLID arg to Campus Solutions' do
+      let(:options) { { fake: true, user_id: user_id } }
+
+      it_should_behave_like 'a proxy that gets data'
+
+      it 'should get specific mock data' do
+        expect(subject[:feed][:root][:higherOneUrl][:url].strip).to eq 'https://commerce.cashnet.com/UCBpaytest?eusername=8062064084e9a8dff7a181266a3ed11e28b80eb30ab4fd84b9bc4de92394d884'
+        expect(proxy.url).to end_with '/UC_OB_HIGHER_ONE_URL_GET.v1/get?EMPLID=26662066'
+      end
+    end
+    context 'pass DELEGATE_UID arg to Campus Solutions' do
+      let(:delegate_uid) { '12350' }
+      let(:options) { { fake: true, user_id: user_id, delegate_uid: delegate_uid } }
+
+      it_should_behave_like 'a proxy that gets data'
+
+      it 'should construct proper URL' do
+        expect(proxy.url).to end_with '/UC_OB_HIGHER_ONE_URL_GET.v1/get?DELEGATE_UID=9000006545'
+      end
     end
   end
 
   context 'real proxy', testext: true do
-    let(:proxy) { CampusSolutions::HigherOneUrl.new(fake: false, user_id: user_id) }
+    let(:proxy) { CampusSolutions::HigherOneUrl.new fake: false, user_id: user_id }
+
     it_should_behave_like 'a proxy that gets data'
   end
 
