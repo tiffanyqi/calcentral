b91a6ee83c056e383a9c0f833213d77d18f399cd
John Crossman <johncrossman@berkeley.edu>
2016-03-07 16:56:01 -0800

    SISRP-15960, My Campus and Financial Resources links are not showing for ex-students

---
+++ spec/models/links/my_campus_links_spec.rb
@@ -0,0 +1,22 @@
+describe Links::MyCampusLinks do
+
+  context 'user roles that can see a particular link' do
+    let(:roles_for_link) {
+      roles_for_link = []
+      roles.each { |role| roles_for_link << double(slug: role) }
+      Links::MyCampusLinks.new.get_roles_for_link double user_roles: roles_for_link
+    }
+    context 'ex-students' do
+      subject { roles_for_link['exStudent'] }
+      context 'student gets a link' do
+        let(:roles) { %w(student) }
+        it { should be true }
+      end
+      context 'no link for student role' do
+        let(:roles) { %w(applicant staff faculty) }
+        it { should be false }
+      end
+    end
+  end
+
+end
