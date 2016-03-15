600bcb33ec11a9d8839fc5682ea411fcfd80ba2a
David Cheng <davidlimcheng@gmail.com>
2016-03-15 14:41:52 -0700

    SISRP-10786 - Front End Dev: Credentials Tab - Work Experience Card - Edit

--- app/models/campus_solutions/my_work_experience.rb
+++ app/models/campus_solutions/my_work_experience.rb
@@ -7,7 +7,7 @@ module CampusSolutions
     def update(params = {})
       params[:startDt] = cs_date_formatter params[:startDt]
       params[:endDt] = cs_date_formatter params[:endDt]
-      if !params[:startDt] or !params[:endDt]
+      if !params[:startDt] || !params[:endDt]
         return err_msg
       end
       passthrough(CampusSolutions::WorkExperience, params)
@@ -19,15 +19,15 @@ module CampusSolutions
 
     def cs_date_formatter(date)
       begin
-        return date if date.blank?
-        return Date.strptime(date, '%m/%d/%Y').strftime('%F')
+        return '' if date.blank?
+        Date.strptime(date, '%m/%d/%Y').strftime('%F')
       rescue ArgumentError
-        return false
+        logger.error "Could not parse date string #{date}"
+        false
       end
     end
 
     def err_msg
-      logger.error "Error reported in back-end validation."
       {
         statusCode: 400,
         errored: true,
