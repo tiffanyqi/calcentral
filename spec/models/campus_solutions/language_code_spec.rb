861dfae2f578f7346b47d4594a9f3057ffb37ff8
John Crossman <johncrossman@berkeley.edu>
2016-03-21 18:10:34 -0700

    SISRP-16631, Afrikaans removed from CampusSolutions:/UC_CC_LANGUAGES

--- spec/models/campus_solutions/language_code_spec.rb
+++ spec/models/campus_solutions/language_code_spec.rb
@@ -1,23 +1,25 @@
 describe CampusSolutions::LanguageCode do
 
   shared_examples 'a proxy that gets data' do
+    let(:proxy) { CampusSolutions::LanguageCode.new fake: fake_proxy }
     subject { proxy.get }
     it_should_behave_like 'a simple proxy that returns errors'
     it_behaves_like 'a proxy that properly observes the profile feature flag'
     it_behaves_like 'a proxy that got data successfully'
     it 'returns data with the expected structure' do
-      expect(subject[:feed][:accomplishments]).to be
-      expect(subject[:feed][:accomplishments][0][:descr]).to eq 'Afrikaans'
+      languages = subject[:feed][:accomplishments].map { |language| language[:descr] }
+      expected_sample = %w(Armenian Hindi/Urdu Yiddish)
+      expect(expected_sample - languages).to be_empty
     end
   end
 
   context 'mock proxy' do
-    let(:proxy) { CampusSolutions::LanguageCode.new(fake: true) }
+    let(:fake_proxy) { true }
     it_should_behave_like 'a proxy that gets data'
   end
 
   context 'real proxy', testext: true do
-    let(:proxy) { CampusSolutions::LanguageCode.new(fake: false) }
+    let(:fake_proxy) { false }
     it_should_behave_like 'a proxy that gets data'
   end
 
