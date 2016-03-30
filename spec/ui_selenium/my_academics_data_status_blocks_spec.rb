bb5a001f7dd2bfeeb971f689392a431d612dae13
Paul Farestveit <pfarestveit@berkeley.edu>
2016-03-30 11:37:32 -0700

    SISRP-16719 - QA residency test update

--- spec/ui_selenium/my_academics_data_status_blocks_spec.rb
+++ spec/ui_selenium/my_academics_data_status_blocks_spec.rb
@@ -11,7 +11,7 @@ describe 'My Academics Status and Blocks', :testui => true do
       testable_users = []
 
       CSV.open(test_output, 'wb') do |user_info_csv|
-        user_info_csv << ['UID', 'Student', 'Ex-Student', 'Registered', 'Resident', 'Active Block', 'Block Types', 'Block History']
+        user_info_csv << ['UID', 'Student', 'Ex-Student', 'Registered', 'Resident', 'Needs Action', 'Active Block', 'Block Types', 'Block History']
       end
 
       test_users.each do |user|
@@ -20,6 +20,7 @@ describe 'My Academics Status and Blocks', :testui => true do
           logger.info("UID is #{uid}")
           api_reg_status = nil
           api_res_status = nil
+          api_res_needs_action = nil
           has_active_block = nil
           popover_block_count = nil
           block_types = nil
@@ -175,6 +176,21 @@ describe 'My Academics Status and Blocks', :testui => true do
 
                   it "shows residency status of '#{my_acad_res_status}' for UID #{uid}" do
                     expect(my_acad_res_status).to eql(api_res_status)
+                  end
+
+                  shows_residency_info_link = my_academics_page.res_info_link?
+                  if api_res_status == 'Non-Resident'
+                    residency_info_link_works = WebDriverUtils.verify_external_link(driver, my_academics_page.res_info_link_element, 'Tuition, Fees, & Residency | Office of the Registrar')
+                    it "shows a 'residency info' link for UID #{uid}" do
+                      expect(shows_residency_info_link).to be true
+                    end
+                    it "offers a valid 'residency info' link for UID #{uid}" do
+                      expect(residency_info_link_works).to be true
+                    end
+                  else
+                    it "shows no 'residency info' link for UID #{uid}" do
+                      expect(shows_residency_info_link).to be false
+                    end
                   end
 
                   if api_res_needs_action == true
@@ -349,7 +365,8 @@ describe 'My Academics Status and Blocks', :testui => true do
             logger.error e.message + "\n" + e.backtrace.join("\n")
           ensure
             CSV.open(test_output, 'a+') do |user_info_csv|
-              user_info_csv << [uid, is_student, is_ex_student, api_reg_status, api_res_status, has_active_block, block_types, has_block_history]
+              user_info_csv << [uid, is_student, is_ex_student, api_reg_status, api_res_status, api_res_needs_action, has_active_block,
+                                block_types, has_block_history]
             end
           end
         end
