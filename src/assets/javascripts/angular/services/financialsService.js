81ef22a57b68fa842c1cfa0baf0f99002edcb186
David Cheng <davidlimcheng@gmail.com>
2016-03-30 15:57:26 -0700

    SISRP-17011 - Create Filtering for Billing Activity

--- src/assets/javascripts/angular/services/financialsService.js
+++ src/assets/javascripts/angular/services/financialsService.js
@@ -8,8 +8,24 @@ angular.module('calcentral.services').service('financialsService', function(anal
     window.print();
   };
 
+  /**
+   * Create JavaScript date object based on the input from the datepicker
+   * @param {String} date Date as a string input
+   * @return {Object | String} Empty string when no date & date object when there is a date
+   */
+  var createDateValues = function(date) {
+    var mmddyyRegex = /^(0[1-9]|1[012])[\/](0[1-9]|[12][0-9]|3[01])[\/]((19|20)\d\d)$/;
+
+    if (date) {
+      var dateValues = date.match(mmddyyRegex);
+      return new Date(dateValues[3], parseInt(dateValues[1], 10) - 1, dateValues[2]);
+    }
+    return '';
+  };
+
   // Expose methods
   return {
-    printPage: printPage
+    printPage: printPage,
+    createDateValues: createDateValues
   };
 });
