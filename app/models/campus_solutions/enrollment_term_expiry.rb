71c47131a7c701b07b63aabc40999ae4c4fd863c
Paul Kerschen <kerschen@berkeley.edu>
2016-04-14 09:55:27 -0700

    SISRP-17790 Expire My Academics semesters on enrollment cache clear

--- app/models/campus_solutions/enrollment_term_expiry.rb
+++ app/models/campus_solutions/enrollment_term_expiry.rb
@@ -3,7 +3,9 @@ module CampusSolutions
     def self.expire(uid=nil)
       [
         MyEnrollmentTerm,
-        MyEnrollmentTerms
+        MyEnrollmentTerms,
+        EdoOracle::UserCourses::All,
+        MyAcademics::Merged
       ].each do |klass|
         klass.expire uid
       end
