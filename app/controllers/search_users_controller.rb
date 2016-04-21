36079b60da8c9219ab474807c137de2b248cac48
John Crossman <johncrossman@berkeley.edu>
2016-04-20 19:49:20 -0700

    SISRP-16088, clean up in adminFactory.js for clear distinction on variety of /api/search_users

--- app/controllers/search_users_controller.rb
+++ app/controllers/search_users_controller.rb
@@ -1,6 +1,13 @@
 class SearchUsersController < ApplicationController
   include ViewAsAuthorization
 
+  before_action :api_authenticate
+
+  rescue_from ArgumentError, with: :handle_api_exception
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  DEFAULT_LIMIT_SEARCH_RESULTS = 50
+
   def search_users
     users_found = authorize_results User::SearchUsers.new(id: id_param).search_users
     render json: { users: users_found }.to_json
@@ -9,6 +16,19 @@ class SearchUsersController < ApplicationController
   def search_users_by_uid
     users_found = authorize_results User::SearchUsersByUid.new(id: id_param).search_users_by_uid
     render json: { users: users_found }.to_json
+  end
+
+  def by_id_or_name
+    filter = {}
+    unless can_view_as_for_all_uids? current_user
+      require_advisor current_user.real_user_id
+      filter = { roles: [:applicant, :student, :recentStudent] }
+    end
+    input = params.require 'input'
+    users = input =~ /\A\d+\z/ ?
+      User::SearchUsers.new(id: input).search_users :
+      User::SearchUsersByName.new.search_by(input, filter)
+    render json: { users: users.take(limit) }.to_json
   end
 
   private
@@ -21,6 +41,12 @@ class SearchUsersController < ApplicationController
 
   def id_param
     params.require 'id'
+  end
+
+  def limit
+    return DEFAULT_LIMIT_SEARCH_RESULTS unless (arg = params[:limit])
+    raise ArgumentError, "The 'limit' param must be a number greater than zero. Invalid: #{arg}" if (limit = arg.to_i) <= 0
+    limit
   end
 
 end
