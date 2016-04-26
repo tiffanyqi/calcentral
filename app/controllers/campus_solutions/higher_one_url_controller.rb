ed9b49660826087c62f741e6f9fe7c9fb29a0080
David Cheng <davidlimcheng@gmail.com>
2016-04-26 15:49:43 -0700

    SISRP-17831 - Add SSO to HigherOne for Students and Delegates for Billing Summary

--- app/controllers/campus_solutions/higher_one_url_controller.rb
+++ app/controllers/campus_solutions/higher_one_url_controller.rb
@@ -6,10 +6,25 @@ module CampusSolutions
     before_filter :authorize_for_financial
 
     def get
+      model = model_from_session
+      render json: model.get_feed_as_json
+    end
+
+    def redirect
+      model = model_from_session
+      if (higher_one_url = model.get_higher_one_url)
+        redirect_to higher_one_url
+      else
+        redirect_to url_for_path '/404'
+      end
+    end
+
+    private
+
+    def model_from_session
       options = {}
       options[:delegate_uid] = current_user.original_delegate_user_id if current_user.authenticated_as_delegate?
-      model = CampusSolutions::MyHigherOneUrl.from_session(session, options)
-      render json: model.get_feed_as_json
+      CampusSolutions::MyHigherOneUrl.from_session(session, options)
     end
 
   end
