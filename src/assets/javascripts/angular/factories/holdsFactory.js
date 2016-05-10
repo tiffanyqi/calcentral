0d98d418b1697404f4b84ed9c94c0c1173adf0f3
David Cheng <davidlimcheng@gmail.com>
2016-05-10 15:46:20 -0700

    SISRP-19036 - 5.4 Student Overview - Enrollment - Ensure Holds are pulling from Student data, not Advisor

--- src/assets/javascripts/angular/factories/holdsFactory.js
+++ src/assets/javascripts/angular/factories/holdsFactory.js
@@ -5,12 +5,14 @@ var angular = require('angular');
 /**
  * Holds Factory
  */
-angular.module('calcentral.factories').factory('holdsFactory', function(apiService) {
+angular.module('calcentral.factories').factory('holdsFactory', function(apiService, $route, $routeParams) {
   // var url = '/dummy/json/holds_empty.json';
   // var url = '/dummy/json/holds_present.json';
-  var url = '/api/campus_solutions/holds';
+  var urlHolds = '/api/campus_solutions/holds';
+  var urlAdvisingStudentHolds = '/api/advising/holds/';
 
   var getHolds = function(options) {
+    var url = $route.current.isAdvisingStudentLookup ? urlAdvisingStudentHolds + $routeParams.uid : urlHolds;
     return apiService.http.request(options, url);
   };
 
