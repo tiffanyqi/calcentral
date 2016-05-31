63f439d88a8628562fe034f9141f840f28467282
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2016-05-31 16:54:27 -0700

    CLC-6328 - Ensure development and test servers point to correct CalCentral instance

--- public/canvas/index.js
+++ public/canvas/index.js
@@ -3,6 +3,14 @@
 
   window.CALCENTRAL = 'https://calcentral.berkeley.edu';
 
+  // Ensure the bCourses development and test servers are pointing to the correct
+  // CalCentral instance when a copy of production is made
+  if (window.location.origin === 'https://ucberkeley.beta.instructure.com') {
+    window.CALCENTRAL = 'https://calcentral-sis-qa.berkeley.edu';
+  } else if (window.location.origin === 'https://ucberkeley.test.instructure.com') {
+    window.CALCENTRAL = 'https://calcentral-qa.berkeley.edu';
+  }
+
   // Load the JavaScript customizations
   $.getScript(window.CALCENTRAL + '/canvas/canvas-customization.js');
 
