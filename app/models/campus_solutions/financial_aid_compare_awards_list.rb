463c264977b280e010c480229fc65f955fd07333
David Cheng <davidlimcheng@gmail.com>
2016-05-31 15:28:25 -0700

    SISRP-20582 - CampusSolutions::FinancialAidCompareAwardsList - NoMethodError undefined method (v77 load test on May 31)

--- app/models/campus_solutions/financial_aid_compare_awards_list.rb
+++ app/models/campus_solutions/financial_aid_compare_awards_list.rb
@@ -19,13 +19,14 @@ module CampusSolutions
       return {} if response.parsed_response.blank?
       feed = response.parsed_response['ROOT'] || response.parsed_response[error_response_root_xml_node] || {}
 
-      feed['AWARD_PARMS']['DATA'] = feed['AWARD_PARMS']['DATA'].sort.reverse.map do |label|
-        {
-           csDate: label,
-           date: format_date(strptime_in_time_zone(label, '%Y-%m-%d-%H.%M.%S'))
-        }
+      if (feed['AWARD_PARMS'])
+        feed['AWARD_PARMS']['DATA'] = feed['AWARD_PARMS']['DATA'].sort.reverse.map do |label|
+          {
+             csDate: label,
+             date: format_date(strptime_in_time_zone(label, '%Y-%m-%d-%H.%M.%S'))
+          }
+        end
       end
-
       feed
     end
 
