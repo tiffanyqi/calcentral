6f08f1a6f661774870aa217101980711f9ccf1ab
John Crossman <johncrossman@berkeley.edu>
2016-07-15 13:39:15 -0700

    NOJIRA, link API added to verify_sis_api_endpoints.sh

--- script/sis/verify_sis_api_endpoints.sh
+++ script/sis/verify_sis_api_endpoints.sh
@@ -209,7 +209,8 @@ verify_crosswalk "/CAMPUS_SOLUTIONS_ID/${CAMPUS_SOLUTIONS_ID}" \
 verify_cs 'always_enabled' true \
   "/UC_AA_ADVISING_RESOURCES.v1/UC_ADVISING_RESOURCES?EMPLID=${CAMPUS_SOLUTIONS_ID}" \
   "/UC_CC_COMM_DB_URL.v1/dashboard/url/" \
-  "/UC_SR_SLR_LINKS.v1/UC_SR_SLR_LINKS_GET"
+  "/UC_SR_SLR_LINKS.v1/UC_SR_SLR_LINKS_GET" \
+  "/UC_LINK_API.v1/get?PROPNAME=UC_CX_LINK"
 
 verify_cs 'cs_profile' "${yml_features_cs_profile}" \
   "/UC_CC_ADDR_LBL.v1/get?COUNTRY=ESP" \
