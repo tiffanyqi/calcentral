23eac7dafe574be2611f4419be71c7a64464a0c5
Tiffany Qi <tiffany19qi@berkeley.edu>
2016-07-21 08:49:56 -0700

    show final exam card, add csv

---
+++ src/assets/javascripts/angular/controllers/widgets/finalExamScheduleController.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var angular = require('angular');
+
+/**
+ * Final exam schedule controller
+ */
+angular.module('calcentral.controllers').controller('finalExamScheduleController', function(apiService, academicsService, myClassesFactory, finalExamScheduleFactory, $scope) {
+
+  
+
+});
