03e5ca726d4322187d532d460b59cd7e97d513f5
Ray Davis <ray@media.berkeley.edu>
2016-08-05 13:59:35 -0700

    SISRP-22377 Revert My Academics Profile data source change, PR #5713

--- src/assets/javascripts/angular/controllers/widgets/transferCreditController.js
+++ src/assets/javascripts/angular/controllers/widgets/transferCreditController.js
@@ -6,10 +6,9 @@ var _ = require('lodash');
 /**
  * Transfer Credit Controller
  */
-angular.module('calcentral.controllers').controller('TransferCreditController', function(apiService, $scope, transferCreditFactory) {
+angular.module('calcentral.controllers').controller('TransferCreditController', function(apiService, $scope, academicsFactory) {
 
   // Temporary MVP functionality, using cumulativeUnits from the academics feed.
-  /*
   var parseCumulativeUnits = function(data) {
     var cumulativeUnits = _.get(data, 'data.collegeAndLevel.cumulativeUnits');
     _.some(cumulativeUnits, function(entry) {
@@ -21,9 +20,9 @@ angular.module('calcentral.controllers').controller('TransferCreditController', 
     });
     $scope.transferCredit.isLoading = false;
   };
-  */
 
   // TransferCredit functionality hidden while CS data API is not complete.
+  /*
   var parseTransferCredit = function(data) {
     var transferCredit = _.get(data, 'data.feed.transferCredit');
     if (transferCredit) {
@@ -32,16 +31,15 @@ angular.module('calcentral.controllers').controller('TransferCreditController', 
     }
     $scope.transferCredit.isLoading = false;
   };
+  */
 
   var loadTransferCredit = function() {
     $scope.transferCredit = {
       isLoading: true,
       cumulativeUnits: ''
     };
-    // cumulativeUnits removed from MyAcademics::CollegeAndLevel as requirement in SISRP-22377
-    // academicsFactory.getAcademics().then(parseCumulativeUnits);
-
-    transferCreditFactory.getTransferCredit().then(parseTransferCredit);
+    // transferCreditFactory.getTransferCredit().then(parseTransferCredit);
+    academicsFactory.getAcademics().then(parseCumulativeUnits);
   };
 
   loadTransferCredit();
